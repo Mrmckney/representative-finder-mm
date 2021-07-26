877a1ecc86984e28332759da55ef73791b7de279
Matthew McKney <mrmckney@gmail.com>
2021-07-26 10:59:25 -0400

    added first state

--- src/components/RepresentativeSearch.js
+++ src/components/RepresentativeSearch.js
@@ -1,10 +1,16 @@
+import { useState } from 'react'
+
 function RepresentativeSearch() {
+    const [address, setAddress] = useState('')
+
     return (
         <div className="search-container" >
             <input 
                 name="representativeSearch" 
                 type="text"
                 className="search-bar"
+                value={address}
+                onChange={e => setAddress(e.target.value)}
             />
         </div>
     )
