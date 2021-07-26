f38484440ee12ffcfed283769e1865e2abcbd49d
Matthew McKney <mrmckney@gmail.com>
2021-07-26 15:30:06 -0400

    added address to officials

--- src/components/RepresentativeSearchResults.js
+++ src/components/RepresentativeSearchResults.js
@@ -2,14 +2,37 @@ function RepresentativeSearchResults(props) {
     
     const {offices, officials} = props
 
+    
 
     return (
         <div>
-            {offices.map(office => 
+            {offices.map(office => {
+                const official = officials[office.officialIndices[0]]
+                const address = official?.address && official?.address[0]
+                return ( 
                 <div className="representative-info">
-                    <div className="office-title" >{office.name}</div>
-                    <p>{officials[office.officialIndices[0]]?.name}</p>
+                    <div className="office-title" >
+                        {office.name}
+                    </div>
+                    <p>
+                        {official?.name}
+                    </p>
+                    <label>Phone:&nbsp;
+                        <span>
+                            {official?.phones[0]}
+                        </span>
+                    </label>
+                    <br />
+                    <label>Address:&nbsp;
+                        <span>
+                            {address?.line1},
+                            &nbsp;{address?.city},
+                            &nbsp;{address?.state},
+                            &nbsp;{address?.zip}
+                        </span>
+                    </label>
                 </div>
+                )}
             )}
 
         </div>
