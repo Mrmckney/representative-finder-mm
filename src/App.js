026e795d3fce4dcee569d577345b85b9776df22e
Matthew McKney <mrmckney@gmail.com>
2021-07-26 10:36:06 -0400

    added RepresentativeSearch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './components/Header'
-import Footer from './components/Footer';
+import Footer from './components/Footer'
+import RepresentativeSearch from './components/RepresentativeSearch'
 
 function App() {
   return (
     <div>
       <Header />
+      <RepresentativeSearch />
       <Footer />
     </div>
   );
---
+++ src/components/RepresentativeSearch.js
@@ -0,0 +1,13 @@
+function RepresentativeSearch() {
+    return (
+        <div className="search-container" >
+            <input 
+                name="representativeSearch" 
+                type="text"
+                className="search-bar"
+            />
+        </div>
+    )
+}
+
+export default RepresentativeSearch
