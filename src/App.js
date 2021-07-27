d035a28a7bfc116aab55d0d0c8dde134338484a5
Matthew McKney <mrmckney@gmail.com>
2021-07-27 10:29:28 -0400

    added router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
 import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import RepresentativeSearch from './components/RepresentativeSearch'
+import SignUp from './components/SignUp'
 
 
 function App() {
   return (
-    <div className="main-page">
-      <Header />
-      <div style={{minHeight: "100vh"}}>
-      <RepresentativeSearch />
+    <Router>
+      <div>
+          <Header />
+            <div style={{minHeight: "100vh"}}>
+              <Switch>
+                <Route path="/signup">
+                    <SignUp />
+                </Route>
+                <Route path="/">
+                    <RepresentativeSearch />
+                </Route>
+              </Switch>
+            </div>
+          <Footer />
       </div>
-      <Footer />
-    </div>
+    </Router>
   );
 }
 
